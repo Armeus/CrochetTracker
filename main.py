febd833a49140d97a27f529478a64653777e2fd0
Jordan Smith <jordan0smith7@gmail.com>
2024-10-15 12:15:55 -0500

    Change structure to utilize dictionary to split and keep track of round number, steps, and total steps

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
+import re
 
 with open("fredHeadAndBodyPattern.txt") as file:
     pattern = file.read()
 
-c_rounds = pattern.splitlines()
-c_rounds_num = len(c_rounds)
+c_pattern = pattern.splitlines()
+c_rounds_num = len(c_pattern)
+c_rounds = []
 
-c_round = c_rounds[1].split()
-s_num = 0
-s_total = 0
-s_type = ''
+for item in c_pattern:
+    r_num = c_pattern.index(item) + 1
+    s_total = int(re.findall(r'\((.*?)\)', item)[0])
+    steps = item.split('(', 1)[0]
+    new_c_round = dict(r_num=r_num, steps=steps, s_total=s_total)
+    c_rounds.append(new_c_round)
 
-for item in c_round:
-    if item.isnumeric():
-        s_num = int(item)
-        continue
-    elif '(' in item:
-        s_total = item.strip('()')
-    else:
-        s_type = item
-
-print(s_num, s_type, s_total)
-
-new_string = ''
-for x in range(s_num):
-    new_string += (s_type + ' ')
-
-print(new_string)
+for c_round in c_rounds:
+    print(c_round['steps'])
